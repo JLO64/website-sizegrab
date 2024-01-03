785030e2d4ff772830dbef7f2d4ec41b4b660e9b
JLO64 <64julianlopez@gmail.com>
2024-01-03 11:58:44 -0800

    added checking for images

--- website-sizegrab.py
+++ website-sizegrab.py
@@ -1,15 +1,30 @@
 import requests, os, shutil
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
-URL = "https://www.julianlopez.net/"
+URL = "https://www.julianlopez.net/about"
 
 # set up a temporary directory
 webdir = "/tmp/website-sizegrab"
 if os.path.exists(webdir): shutil.rmtree(webdir)
-os.makedirs(webdir, exist_ok=True)
+os.makedirs(webdir)
+os.makedirs(webdir + "/img")
 os.chdir(webdir)
 
 # Grab HTML
 response = requests.get(URL)
 html = response.text
-with open("page.html", 'w') as f:
-    f.write(html)
+with open("page.html", 'w') as f: f.write(html)
+
+# Parse HTML for images
+os.chdir(webdir + "/img")
+soup = BeautifulSoup(html, 'html.parser')
+img_tags = soup.find_all('img')
+for img in img_tags:
+    img_url = img.attrs.get("src")
+    if not img_url: continue
+    img_url = urljoin(URL, img_url)
+    basename = os.path.basename(img_url)
+    img_res = requests.get(img_url)
+    with open(basename, 'wb') as f: f.write(img_res.content)
+os.chdir(webdir)
